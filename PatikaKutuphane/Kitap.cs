56dcc70a912a811ddd3cb949f331cb1ee64a4682
selinozluk <selinozluk36@gmail.com>
2025-07-13 15:46:11 +0300

    Add project files.

---
+++ PatikaKutuphane/Kitap.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace PatikaKutuphane
+{
+    // Classlar nesne oluşturma şablonudur
+    internal class Kitap
+    {
+        // Propertyler: Nesnenin sahip olacağı verileri tutar.
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorSurname { get; set; }
+        public int PageCount { get; set; }
+        public string Publisher { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        // Default constructor – parametre almadan nesne oluşturur
+        public Kitap()
+        {
+            RegisterDate = DateTime.Now; // Nesne oluşturulduğu an kayıt tarihi atanır
+        }
+
+        // Alternatif constructor – parametre alır, değerleri direkt atar
+        public Kitap(string title, string authorName, string authorSurname, int pageCount, string publisher)
+        {
+            Title = title;
+            AuthorName = authorName;
+            AuthorSurname = authorSurname;
+            PageCount = pageCount;
+            Publisher = publisher;
+            RegisterDate = DateTime.Now;
+        }
+
+        // Kitap bilgilerini yazdıran metot
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {AuthorName} {AuthorSurname}");
+            Console.WriteLine($"Page Count: {PageCount}");
+            Console.WriteLine($"Publisher: {Publisher}");
+            Console.WriteLine($"Register Date: {RegisterDate.ToShortDateString()}");
+            Console.WriteLine("-----------------------------------");
+        }
+    }
+}
---
+++ PatikaKutuphane/Program.cs
@@ -0,0 +1,25 @@
+﻿using PatikaKutuphane;
+
+internal class Program
+{
+     static void Main(string[] args)
+    {
+
+        // Default constructor ile nesne oluşturduk
+        Kitap book1 = new Kitap
+        {
+            Title = "Adı Aylin",
+            AuthorName = "Ayşe",
+            AuthorSurname = "Kulin",
+            PageCount = 398,
+            Publisher = "Remzi Kitabevi"
+        };
+
+        // Parametreli constructor ile direkt değer atanmış nesne
+        Kitap book2 = new Kitap("Adı Aylin", "Ayşe", "Kulin", 398, "Remzi Kitabevi");
+
+        // Kitap bilgilerini ekrana yazdırır
+        book1.PrintInfo();
+        book2.PrintInfo();
+    }
+}
